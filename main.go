481bea43bfb7dbf3a0dff0841eb9868b87b86783
MayankGandhe <mayank.gandhe@gmail.com>
2023-06-04 17:29:57 +0530

    Updated code to gin framework

--- main.go
+++ main.go
@@ -1,47 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r := gin.Default()
+
+	r.Use(func(c *gin.Context) {
 		// Print request method and URL
-		fmt.Printf("Received request: %s %s\n", r.Method, r.URL.Path)
-		fmt.Printf("Got Input")
+		fmt.Printf("Received request: %s %s\n", c.Request.Method, c.Request.URL.Path)
 
 		// Print request headers
 		fmt.Println("Headers:")
-		for name, values := range r.Header {
+		for name, values := range c.Request.Header {
 			for _, value := range values {
 				fmt.Printf("%s: %s\n", name, value)
 			}
 		}
 
-		// Read request body
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println("Error reading request body:", err)
-
-			// Send an error response
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-		defer r.Body.Close()
-
-		// Print request body
-		fmt.Println("Body:")
-		fmt.Println(string(body))
-
-		// You can add your additional logic here
-
-		// Example response
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+		// Continue processing the request
+		c.Next()
 	})
-	fmt.Println("Server Started")
-	log.Error(http.ListenAndServe(":5000", nil))
+
+	r.GET("/", func(c *gin.Context) {
+		// Convert headers to map[string]string
+		headers := make(map[string]string)
+		for name, values := range c.Request.Header {
+			headers[name] = values[0]
+		}
+
+		// Set Content-Type header
+		c.Header("Content-Type", "application/json")
+
+		// Return headers in JSON format
+		c.JSON(http.StatusOK, headers)
+	})
+
+	log.Fatal(r.Run(":5000"))
 }

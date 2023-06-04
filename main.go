d598f344d1b5806ace21e0cad495e563698a5c56
MayankGandhe <mayank.gandhe@gmail.com>
2023-06-04 16:05:16 +0530

    Updated code

--- main.go
+++ main.go
@@ -1,38 +1,46 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-type DebugHandler struct{}
-
-func (dh DebugHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Received request: %s %s", r.Method, r.URL.Path)
-
-	// Convert headers to map[string]string
-	headers := make(map[string]string)
-	for name, values := range r.Header {
-		headers[name] = values[0]
-	}
-
-	// Indent headers map to JSON
-	headersJSON, err := json.MarshalIndent(headers, "", "  ")
-	if err != nil {
-		log.Println("Error encoding headers to JSON:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	// Set Content-Type header
-	w.Header().Set("Content-Type", "application/json")
-
-	// Write JSON response
-	w.Write(headersJSON)
-}
-
 func main() {
-	http.Handle("/", DebugHandler{})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Print request method and URL
+		fmt.Printf("Received request: %s %s\n", r.Method, r.URL.Path)
+
+		// Print request headers
+		fmt.Println("Headers:")
+		for name, values := range r.Header {
+			for _, value := range values {
+				fmt.Printf("%s: %s\n", name, value)
+			}
+		}
+
+		// Read request body
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Error reading request body:", err)
+
+			// Send an error response
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+
+		// Print request body
+		fmt.Println("Body:")
+		fmt.Println(string(body))
+
+		// You can add your additional logic here
+
+		// Example response
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }

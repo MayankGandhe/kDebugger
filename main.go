d9018a7f6c67fbcb827b374fc403c4d5063bcd4c
MayankGandhe <mayank.gandhe@gmail.com>
2023-06-04 15:30:49 +0530

    Updated code to return it to response

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -10,21 +11,28 @@ type DebugHandler struct{}
 func (dh DebugHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received request: %s %s", r.Method, r.URL.Path)
 
-	// Log all headers
-	for name, headers := range r.Header {
-		for _, h := range headers {
-			log.Printf("Header: %s: %s", name, h)
-		}
+	// Convert headers to map[string]string
+	headers := make(map[string]string)
+	for name, values := range r.Header {
+		headers[name] = values[0]
 	}
 
-	// Add your additional logic here
+	// Convert headers map to JSON
+	headersJSON, err := json.Marshal(headers)
+	if err != nil {
+		log.Println("Error encoding headers to JSON:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 
-	// Example response
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	// Set Content-Type header
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write JSON response
+	w.Write(headersJSON)
 }
 
 func main() {
 	http.Handle("/", DebugHandler{})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
